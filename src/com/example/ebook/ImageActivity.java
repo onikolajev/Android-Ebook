fcdfdcc6b4adec82a90f97617e34232f0eb2bf1b
sw1ch <olegoleg17@gmail.com>
2014-03-08 11:43:26 +0200

    Immutable source problem fix

--- src/com/example/ebook/ImageActivity.java
+++ src/com/example/ebook/ImageActivity.java
@@ -1,11 +1,15 @@
 package com.example.ebook;
 
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,7 @@ import android.widget.Toast;
 
 public class ImageActivity extends Activity {
 
+	private static final int REQUEST_CODE = 1;
 	private static final String TAG = "sw1ch";
 	private Bitmap mBitmap;
 	private Bitmap mImage;
@@ -25,12 +30,13 @@ public class ImageActivity extends Activity {
 	private int fY;
 	private int sX;
 	private int sY;
+	private Resources res;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image);
-		Resources res = getResources();
+		res = getResources();
 
 		Log.d(TAG, "Activity started!");
 		// create bitmap 32
@@ -54,12 +60,17 @@ public class ImageActivity extends Activity {
 	public void onWindowFocusChanged(boolean hasFocus) {
 		maxX = mCanvas.getWidth();
 		maxY = mCanvas.getHeight();
-		Log.d(TAG, "Dimmensions: " + maxX + " " + maxY);
+		String mString;
+		if (hasFocus)
+			mString = "Window focus received";
+		else
+			mString = "Window focus lost";
+		Log.d(TAG, mString);
+
 	}
 
 	public void drawLine(View v) {
 		// First point coords
-		Log.d(TAG, "Line will be drawn");
 		fX = (int) (Math.random() * (maxX + 1));
 		fY = (int) (Math.random() * (maxY + 1));
 		// Second point coords
@@ -79,7 +90,7 @@ public class ImageActivity extends Activity {
 
 		// From top left to bottom right
 		mCanvas.drawLine(fX, fY, sX, sY, mPaint);
-		
+
 		// draw updated bitmap
 		mImageView.setImageBitmap(mBitmap);
 		Log.d(TAG, "Line was drawn on canvas! [" + fX + ", " + fY + ",] and ["
@@ -93,7 +104,7 @@ public class ImageActivity extends Activity {
 		int dXY = (int) (Math.sqrt(Math.pow((sX - fX), 2)
 				+ Math.pow((sY - fY), 2)));
 		Log.d(TAG, "dXY: " + dXY);
-		
+
 		if (dXY > 200)
 			return 2;
 		else if ((dXY <= 200) && (dXY > 100))
@@ -102,10 +113,82 @@ public class ImageActivity extends Activity {
 			return 0;
 
 	}
-	
-	public void clearCanvas(View v){
+
+	public void clearCanvas(View v) {
 		Log.d(TAG, "Canvas was cleared!");
 		mCanvas.drawColor(0xff000000);
 		mImageView.setImageBitmap(mBitmap);
+	}
+
+	public void chooseBitmap(View v) {
+		Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+		i.setType("image/*");
+		Intent iChooser = Intent.createChooser(i, "Select");
+		startActivityForResult(iChooser, REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+			// get URI for data
+			Uri uri = data.getData();
+			Log.d(TAG, uri.toString());
+			Toast.makeText(getApplicationContext(), uri.toString(),
+					Toast.LENGTH_LONG).show();
+			
+			try {
+				
+				// New stream with data URI
+				InputStream stream = getContentResolver().openInputStream(uri);
+				
+				// Get options 
+				BitmapFactory.Options options = new BitmapFactory.Options();
+				options.inJustDecodeBounds = true;			
+				Log.d(TAG, "Options created");
+				
+				// Get Bitmap from stream with edited options
+				BitmapFactory.decodeStream(stream, null, options);
+				stream.close();
+
+				
+				// get dimensioms
+				int displayW = getResources().getDisplayMetrics().widthPixels;
+				int displayH = getResources().getDisplayMetrics().heightPixels;
+				Log.d(TAG, "Display size:" + displayH + " x " + displayW);
+				int w = options.outWidth;
+				int h = options.outHeight;	
+				Log.d(TAG, "Bitmap raw size:" + w + " x " + h);
+				
+				//Sampling
+				int sample = 1;
+				while (w > displayW * sample || h > displayH * sample) {
+					sample = sample * 2;
+				}
+				Log.d(TAG, "Sampling at " + sample);
+				
+				//
+				stream = getContentResolver().openInputStream(uri);				
+				options.inJustDecodeBounds = false;
+				options.inSampleSize = sample;
+				
+				// Get Bitmap from stream with edited options
+				Bitmap iBitmap = BitmapFactory.decodeStream(stream, null, options);
+				mBitmap = iBitmap.copy(Bitmap.Config.ARGB_8888, true);
+				
+				
+				//ImageView v = (ImageView) findViewById(R.id.image12);
+				//Add new canvas to the Bitmap
+				mCanvas = new Canvas(mBitmap);
+				mCanvas.drawColor(0x00000000);
+				mImageView.setImageBitmap(mBitmap);
+				Log.d(TAG, "Bitmap decoded successfully");
+
+			} catch (Exception e) {
+				Log.e(TAG, "Decoding error:", e);
+			}
+;
+		}
 	}
 }
