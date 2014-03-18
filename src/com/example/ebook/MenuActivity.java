4b47b263215538d9cd78f0b6ef0ab1840e0a7a4a
sw1ch <olegoleg17@gmail.com>
2014-03-18 12:45:56 +0200

    ActionBarSherlock added + new new style of app.

--- src/com/example/ebook/MenuActivity.java
+++ src/com/example/ebook/MenuActivity.java
@@ -1,34 +1,44 @@
 package com.example.ebook;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 
+public class MenuActivity extends SherlockActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
 	}
 
-	public void goToBook (View v){
-		Intent i = new Intent (this, MainActivity.class);
-		startActivity(i);
-		
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 
-	public void goToForm (View v){
-		Intent i = new Intent (this, SubActivity.class);
+	public void goToBook(View v) {
+		Intent i = new Intent(this, MainActivity.class);
+		startActivity(i);
+
+	}
+
+	public void goToForm(View v) {
+		Intent i = new Intent(this, SubActivity.class);
 		startActivity(i);
 	}
-	
-	public void goToMusic(View v){
+
+	public void goToMusic(View v) {
 		Intent i = new Intent(this, MusicActivity.class);
 		startActivity(i);
 	}
-	
+
 	public void goToImage(View v) {
 		// TODO Auto-generated method stub
 		Intent i = new Intent(this, ImageActivity.class);
