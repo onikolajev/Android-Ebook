1655e6585a113bcea8e78b0a0dcb61a14846db83
sw1ch <olegoleg17@gmail.com>
2014-03-18 11:27:30 +0200

    GraphView last style changes

--- gen/com/example/ebook/R.java
+++ gen/com/example/ebook/R.java
@@ -22,7 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int me_mf=0x7f020001;
+        public static final int ic_menu_statistics=0x7f020001;
+        public static final int me_mf=0x7f020002;
     }
     public static final class id {
         public static final int BookB=0x7f09000a;
--- src/com/example/ebook/StatsActivity.java
+++ src/com/example/ebook/StatsActivity.java
@@ -17,43 +17,51 @@ public class StatsActivity extends Activity {
 	private static final String KEY_SHORT = "Short";
 	private static final String KEY_MEDIUM = "Medium";
 	private static final String KEY_LONG = "Long";
-
+	int countLong;
+	int countMedium;
+	int countShort;
 	private static final String TAG = "sw1ch";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stats);
 		SharedPreferences mPrefs = getSharedPreferences("stats", MODE_PRIVATE);
-		int countLong = mPrefs.getInt(KEY_LONG, 0);
-		int countMedium = mPrefs.getInt(KEY_MEDIUM, 0);
-		int countShort = mPrefs.getInt(KEY_SHORT, 0);
-
-		Log.d(TAG, "Short: " + mPrefs.getInt(KEY_SHORT, -1));
-		Log.d(TAG, "Medium: " + mPrefs.getInt(KEY_MEDIUM, -1));;
-		Log.d(TAG, "Long: " + mPrefs.getInt(KEY_LONG, -1));
 		
+		//getting data from preferences 
+		//and choosing the maximum value
+		countLong = mPrefs.getInt(KEY_LONG, 0);
+		countMedium = mPrefs.getInt(KEY_MEDIUM, 0);
+		countShort = mPrefs.getInt(KEY_SHORT, 0);
+		Log.d(TAG, "Short: " + countShort + "\nMedium: " 
+		+ countMedium + "\nLong: " + countLong);
+		
+		int max = countLong;
+		if (max <= countMedium) max = countMedium;
+		if (max <= countShort) max = countShort;
+		Log.d(TAG, "Maximal value: "+max);
 
+		// Data arrangement
 		GraphViewSeries mGraphSeries = new GraphViewSeries(new GraphViewData[] {
 				new GraphViewData(0, countShort),
 				new GraphViewData(1, countMedium),
-				new GraphViewData(2, countLong),
-				new GraphViewData(3, 0)});
+				new GraphViewData(2, countLong)
+				});
 
 		GraphView graphView = new BarGraphView(this, "Line Statistics");
 		graphView.addSeries(mGraphSeries);
-		graphView.setHorizontalLabels(new String[] { "Low", "Medium", "Short", "" });
+		graphView.setHorizontalLabels(new String[] { "Short", "Medium", "Long"});
 		graphView.getGraphViewStyle().setNumHorizontalLabels(3);
-		// graphView.setVerticalLabels(new String[]{"Low", "Medium", "Short"});
-		
-		//Style
+		graphView.setManualYAxisBounds(max, 0);
+
+		// Style
 		graphView.getGraphViewStyle().setTextSize(12);
 		graphView.setBackgroundColor(Color.BLACK);
 		graphView.getGraphViewStyle().setGridColor(Color.BLACK);
 		graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.WHITE);
 		graphView.getGraphViewStyle().setVerticalLabelsColor(Color.WHITE);
 		((BarGraphView) graphView).setDrawValuesOnTop(true);
-		
+
 		LinearLayout ll = (LinearLayout) findViewById(R.id.graph);
 		ll.addView(graphView);
 	}
